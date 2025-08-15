b7a7fd6283c7c5c94ff1f24f63f48813cfece0b4
Mohan Lal <mhnuk2007@gmail.com>
2025-08-15 22:58:22 +0500

    Add Spring Boot app with Dockerfile and config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student(name, age) VALUES ('Sandeep', 20);
+INSERT INTO student(name, age) VALUES ('Honey', 9);
+INSERT INTO student(name, age) VALUES ('Munesh', 22);
---
+++ src/test/java/com/learning/studentapp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.studentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
